d2c4fed14b6435c72130345d52c7bf7369bac5aa
fiovargas <fiovargasp31@gmail.com>
2025-10-01 22:58:44 -0600

    actualizacion 9

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState }  from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import ServicesAdmin from '../../services/ServicesAdmin';
+import './Login.css'
+
 
 function Login() {
+
+  const navegar = useNavigate();
+  const [nombre,setNombre]=useState("")
+  const [password,setPassword]=useState("")
+
+    useEffect(() => {
+    const usuario = localStorage.getItem("usuario")
+    if (usuario) {
+      navegar("/AdminR", { replace: true })
+    }
+  }, [navegar])
+
+
+    const iniciarSesion = async () => {
+
+      if (!nombre || !password) {
+      toast.error("Debes completar todos los campos");
+      return;
+    }
+
+    if (nombre.length < 3) {
+          toast.error("El nombre debe tener mínimo 3 caracteres");
+          return;
+        }
+    
+        if (password.length < 8) {
+          toast.error("La contraseña debe tener mínimo 8 caracteres");
+          return;
+        }
+
+     try {
+      const usuarios = await ServicesAdmin.getUsers();
+      const usuarioEncontrado = usuarios.find(
+        (user) => user.nombre === nombre && user.password === password
+      );
+
+      if (usuarioEncontrado) {
+        localStorage.setItem("usuario", JSON.stringify({
+
+          id: usuarioEncontrado.id,
+          nombre: usuarioEncontrado.nombre,
+        }))
+        
+        toast.success("Inicio de sesión exitoso", { toastId: "loginExitoso" });
+        navegar("/AdminR", {replace: true});
+      } else {
+        toast.error("Usuario o contraseña incorrectos", { toastId: "loginError" });
+      }
+
+    } catch (error) {
+      toast.error("Error al conectar con el servidor", { toastId: "serverError" });
+      console.error("Error en login:", error);
+    }
+  };
+  
+
   return (
     <div>
-      Login
+      
+      <div>
+        <div className='auth-contenedor'>
+
+          <h2>Inicio de sesión</h2>
+
+          <label className='nombre' htmlFor="nombre">Nombre de usuario</label>
+          <input type="text" id='nombre' placeholder='Nombre de usuario' value={nombre} onChange={(e)=>setNombre(e.target.value)} />
+          
+          <label className='password' htmlFor="Contraseña">Contraseña</label>
+          <input type="password" id='password' placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)} />
+          
+          <button className='auth-button' onClick={iniciarSesion} >Iniciar sesión</button>
+        </div>
+      </div>
+
+  
+
     </div>
   )
 }
